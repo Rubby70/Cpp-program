7b642bf5c081db33fa1582872a16e1e5cec33f2d
bobo <bear19990220@gmail.com>
2024-11-09 12:09:46 +0800

    1109-2

--- 複習複習複習/Sorting.cpp
+++ 複習複習複習/Sorting.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 
+int* RandomArray(){
+
+    int length = 10;
+    int* ptr = new int(length);
+    for(int i = 0; i < length; i ++){
+        ptr[i] = rand() % 10;
+    }
+    return ptr;
+
+}
+
 int main(void){
 
+    srand(time(0));
+    int* arr1 = RandomArray();
+    cout << arr1 << "\n";
     return 0;
     
 }
