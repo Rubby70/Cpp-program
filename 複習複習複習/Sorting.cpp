90976eed5876b9010beac69aa046d83d40fb58b6
bear19990220 <bear19990220@gmail.com>
2024-11-10 17:21:26 +0800

    1110

--- 複習複習複習/Sorting.cpp
+++ 複習複習複習/Sorting.cpp
@@ -2,22 +2,63 @@
 using namespace std;
 
 
-int* RandomArray(){
+int* RandomArray(int length){
 
-    int length = 10;
-    int* ptr = new int(length);
+    int* ptr = new int[length];
     for(int i = 0; i < length; i ++){
         ptr[i] = rand() % 10;
     }
     return ptr;
 
 }
+int* CopyArray(int* arr1, int length){
+
+    int* arr2 = new int[length];
+    for(int i = 0; i < length; i ++){
+        arr2[i] = arr1[i];
+    }
+    return arr2;
+
+}
+void PrintArray(int* arr, int length){
+
+    cout << "[";
+    for(int i = 0; i < length; i ++){
+        cout << arr[i] << " ";
+    }
+    cout << "]\n";
+
+}
+void CountingSort(int* arr, int length){
+
+    cout << "排序前: ";
+    PrintArray(arr, length);
+
+    int max = 0;
+    for(int i = 0; i < length; i ++){
+        if(arr[i] > max)
+            max = arr[i];
+    }
+    cout << max << "\n";
+
+    int CountingArray[max];
+
+}
+void BubbleSort(int* arr, int length){}
+void InsertionSort(int* arr, int length){}
+void MergeLoop(int* arr, int length, int start, int mid, int end){}
+void MergeSort(int* arr, int length, int start, int end){}
 
 int main(void){
 
     srand(time(0));
-    int* arr1 = RandomArray();
-    cout << arr1 << "\n";
+    int length = 10;
+    int* arr1 = RandomArray(length);
+    int* arr2 = CopyArray(arr1, length);
+    int* arr3 = CopyArray(arr1, length);
+    int* arr4 = CopyArray(arr1, length);
+
+    CountingSort(arr1, length);
     return 0;
     
 }
